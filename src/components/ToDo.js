a0aad00b1a2499f687a6f04236db2f49748075f7
Carlitos <basilecarlos12@gmail.com>
2023-10-25 08:42:18 +0200

    Todo basic functinonality completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
-const ToDo = () => {
+const ToDo = ({task}) => {
+
   return (
-    <div>ToDo</div>
+    console.log("SONO QUI"),
+    <div className='Todo'>
+      <p>{task.task}</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} className='trash' />
+        <FontAwesomeIcon icon={faTrash} className='edit' />
+      </div>  
+    </div>
+    
   )
+
 }
 
 export default ToDo
--- src/components/ToDoWrapper.js
+++ src/components/ToDoWrapper.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import ToDoForm from './ToDoForm'
+import ToDo from './ToDo'
 import {v4 as uuidv4} from 'uuid'
 
 uuidv4();
@@ -9,14 +10,21 @@ const ToDoWrapper = () => {
 
   const addTodo = (todo) => {
     setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
-    console.log(todos)
+    console.log("AGGIUNTO")
   }
 
   return (
 
-    
   <div className='TodoWrapper'>
+      <h1>Get things done!</h1>
       < ToDoForm addTodo={addTodo} />
+      {todos.map((todo) =>
+          <ToDo
+            key={todo.id}
+            task={todo}
+          />
+        )
+      }
     </div>
   )
 }
